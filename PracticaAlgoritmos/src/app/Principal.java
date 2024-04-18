2d7dab0bf4d23fc4a817e4ba2124cc98e6bb6234
WoliBlue <162577876+WoliBlue@users.noreply.github.com>
2024-04-18 20:07:20 +0200

    añadida principal sin nada

---
+++ PracticaAlgoritmos/src/app/Principal.java
@@ -0,0 +1,18 @@
+package app;
+
+import util.Algoritmos;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		
+		
+		
+		
+	}
+
+}
--- PracticaAlgoritmos/src/util/Algoritmos.java
+++ PracticaAlgoritmos/src/util/Algoritmos.java
@@ -7,6 +7,7 @@ public abstract class Algoritmos {
      * @return La formula Fibonacci.
      */
     public static int fibonacci(int num) {
+    	//Recibe un número entero y devuelve el número de fibonacci de dicho número.
         if (num <= 1)
             return num;
         return fibonacci(num - 1) + fibonacci(num - 2);
@@ -17,6 +18,7 @@ public abstract class Algoritmos {
      * @return La formula factorial.
      */
     public static int factorial(int num) {
+    	//  Recibe un número entero y devuelve el factorial de dicho número
         int res = 1;
         for (int i = 1; i <= num; i++) {
             res *= i;
@@ -26,9 +28,10 @@ public abstract class Algoritmos {
 	
     /**
      * @param primo
-     * @return La formula de si es primo es true.
+     * @return La formula de si es primo
      */
     public static boolean primo(int num) {
+    	// Recibe un número entero y devuelve true o false en función de si es primo o no.
         if (num <= 1)
             return false;
         for (int i = 2; i <= Math.sqrt(num); i++) {
